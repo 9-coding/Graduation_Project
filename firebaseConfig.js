fe387900fb8aa045e8ebd886623a3afddda4f3c8
SeohyunSim <angelsh0805@gachon.ac.kr>
2023-12-01 03:49:17 +0900

    error

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,12 @@
-import { initializeApp } from "firebase/compat/app";
-import "firebase/compat/database";
-import "firebase/compat/storage";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyD7jlUzKiSs6oLOMptBnweP8XhrOuiUyZ8",
     authDomain: "cloc-bdf74.firebaseapp.com",
@@ -15,3 +20,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- src/AddScreen.js
+++ src/AddScreen.js
@@ -1,13 +1,116 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Button, StyleSheet, Text, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+//import firebase from 'firebase/app';
+//import 'firebase/storage';
+import { storage } from "../firebaseConfig";
+import { ref, uploadBytes, getDownloadURL } from 'firebase/compat/storage';
 
-const AddScreen = () => {
+
+
+
+export default function ImageUploadScreen() {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    //권한 허용
+    useEffect(() => {
+        (async () => {
+            if (Platform.OS !== 'web') {
+                const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+                const mediaLibraryStatus = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+                if (cameraStatus.status !== 'granted' || mediaLibraryStatus.status !== 'granted') {
+                    alert('Permission to access camera and media library is required!');
+                }
+            }
+        })();
+    }, []);
+
+    //갤러리에서 사진 선택
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri);
+        }
+
+    };
+
+    //카메라로 촬영
+    const takePicture = async () => {
+        let result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri);
+        }
+    };
+
+    //이미지 업로드
+    const uploadImage = async () => {
+        try {
+            const response = await fetch(selectedImage);
+            const blob = await response.blob();
+
+            // Create a reference to the storage bucket
+            const storageRef = firebase.storage().ref();
+
+            // Create a unique filename for the image
+            const filename = `${Date.now()}.jpg`;
+
+            // Upload the image to Firebase Storage
+            const uploadTask = storageRef.child(`Cloth/${filename}`).put(blob);
+
+            uploadTask.on(
+                'state_changed',
+                (snapshot) => {
+                    // Track upload progress if needed
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log(`Upload is ${progress}% done`);
+                },
+                (error) => {
+                    console.error('Error uploading image:', error);
+                },
+                () => {
+                    // Handle successful upload
+                    console.log('Image uploaded successfully!');
+                }
+            );
+        } catch (error) {
+            console.error('Error preparing image for upload:', error);
+        }
+    };
+
+   
+
     return (
-        <View>
-            <Text>Welcome to the Add Screen!</Text>
-            {/* Add your specific content and components here */}
+        <View style={styles.container}>
+            <Button title="Take Picture" onPress={takePicture} />
+            <Button title="Pick Image from Gallery" onPress={pickImage} />
+            <Button title="Upload Image" onPress={uploadImage} />
+            {selectedImage && <Image source={{ uri: selectedImage }} style={styles.image} />}
         </View>
     );
-};
+}
 
-export default AddScreen;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: 200,
+        height: 200,
+        marginTop: 20,
+    },
+});
